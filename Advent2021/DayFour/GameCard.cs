c39022ff3bc5b28f8970ff9f26b95bebc329872d
Brian Nolan <bnolan001@gmail.com>
2021-12-05 23:00:38 -0500

    Completed day 4

---
+++ Advent2021/DayFour/GameCard.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayFour
+{
+    public class GameCard
+    {
+        private List<HashSet<int>> rows;
+        private List<HashSet<int>> cols;
+        private HashSet<int> numbers;
+        private List<int> matchedNumbers;
+        private List<int> unmatchedNumbers;
+
+        public GameCard()
+        {
+            rows = new List<HashSet<int>>();
+            cols = new List<HashSet<int>>();
+            numbers = new HashSet<int>();
+            matchedNumbers = new List<int> ();
+            unmatchedNumbers = new List<int> ();
+        }
+
+        public void AddRow(List<int> row)
+        {
+            if (cols.Count == 0)
+            {
+                foreach(var i in row)
+                {
+                    cols.Add(new HashSet<int>());
+                }
+            }
+            var hashRow = new HashSet<int>();
+            for(var idx = 0; idx < row.Count; idx++)
+            {
+                hashRow.Add(row[idx]);
+                cols[idx].Add(row[idx]);
+                unmatchedNumbers.Add(row[idx]);
+                numbers.Add(row[idx]);
+            }
+            rows.Add(hashRow);
+        }
+
+        public bool CheckForNumber(int num)
+        {
+            bool winner = false;
+            if (numbers.Contains(num))
+            {
+                matchedNumbers.Add(num);
+                unmatchedNumbers.Remove(num);
+                foreach(var row in rows)
+                {
+                    if (row.Remove(num))
+                    {
+                        winner |= row.Count == 0;
+                    }
+                }
+
+                foreach(var col in cols)
+                {
+                    if (col.Remove(num))
+                    {
+                        winner |= col.Count == 0;
+                    }
+                }
+            }
+            return winner;
+        }
+
+        public bool IsValidCard()
+        {
+            return rows.Count > 0 && cols.Count > 0;
+        }
+
+        public int CalculateScore(int num)
+        {
+            var score = unmatchedNumbers.Sum() * num;
+
+            return score;
+        }
+    }
+}
---
+++ Advent2021/DayFour/Program.cs
@@ -0,0 +1,119 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DayFour;
+
+ProblemOne();
+Console.WriteLine("-----------------------------");
+ProblemTwo();
+Console.WriteLine("-----------------------------");
+Console.WriteLine("Completed Day 4");
+
+static void ProblemOne()
+{
+    Console.WriteLine("Day 4 Problem 1");
+
+    var data = File.ReadAllLines("game.txt");
+
+    var playedNumbers = data[0].Split(',').Select(d => int.Parse(d)).ToList();
+    var gameCard = new GameCard();
+    var gameCards = new List<GameCard>(); ;
+    for (var idx = 2; idx < data.Length; idx++)
+    {
+        if (String.IsNullOrWhiteSpace(data[idx]))
+        {
+            if (gameCard.IsValidCard())
+            {
+                gameCards.Add(gameCard);
+            }
+            gameCard = new GameCard();
+            continue;
+        }
+
+        var row = data[idx].Split(' ').Where(d => d.Trim().Length > 0).Select(d => int.Parse(d)).ToList();
+        gameCard.AddRow(row);
+    }
+
+    if (gameCard.IsValidCard())
+    {
+        gameCards.Add(gameCard);
+    }
+
+    var winningCards = new List<GameCard>();
+    var winningNumber = -1;
+    foreach (var num in playedNumbers)
+    {
+        foreach (var card in gameCards)
+        {
+            if (card.CheckForNumber(num))
+            {
+                winningCards.Add(card);
+            }
+        }
+        if (winningCards.Count > 0)
+        {
+            winningNumber = num;
+            break;
+        }
+    }
+    foreach (var card in winningCards) {
+        Console.WriteLine($"Winning card score {card.CalculateScore(winningNumber)}");
+    }
+}
+
+static void ProblemTwo()
+{
+    Console.WriteLine("Day 4 Problem 2");
+
+    var data = File.ReadAllLines("game.txt");
+
+    var playedNumbers = data[0].Split(',').Select(d => int.Parse(d)).ToList();
+    var gameCard = new GameCard();
+    var gameCards = new List<GameCard>(); ;
+    for (var idx = 2; idx < data.Length; idx++)
+    {
+        if (String.IsNullOrWhiteSpace(data[idx]))
+        {
+            if (gameCard.IsValidCard())
+            {
+                gameCards.Add(gameCard);
+            }
+            gameCard = new GameCard();
+            continue;
+        }
+
+        var row = data[idx].Split(' ').Where(d => d.Trim().Length > 0).Select(d => int.Parse(d)).ToList();
+        gameCard.AddRow(row);
+    }
+
+    if (gameCard.IsValidCard())
+    {
+        gameCards.Add(gameCard);
+    }
+
+    var winningCards = new List<GameCard>();
+    var winningNumber = -1;
+    foreach (var num in playedNumbers)
+    {
+        foreach (var card in gameCards)
+        {
+            if (card.CheckForNumber(num))
+            {
+                winningCards.Add(card);
+            }
+        }
+        foreach(var winner in winningCards)
+        {
+            gameCards.Remove(winner);
+        }
+        
+        if (gameCards.Count == 0)
+        {
+            winningNumber = num;
+            break;
+        }
+        winningCards.Clear();
+    }
+    foreach (var card in winningCards)
+    {
+        Console.WriteLine($"Winning card score {card.CalculateScore(winningNumber)}");
+    }
+}
