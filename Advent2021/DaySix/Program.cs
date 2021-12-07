4ecbb409bfe2d84bd84bc41eee49475ec3230e0d
Brian Nolan <bnolan001@gmail.com>
2021-12-06 21:49:34 -0500

    Added day 6 project with solved problem 1 and possible problem 2 solution

---
+++ Advent2021/DaySix/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+ProblemOne();
+Console.WriteLine("-----------------------------");
+ProblemTwo();
+Console.WriteLine("-----------------------------");
+Console.WriteLine("Completed Day 6");
+
+static void ProblemOne()
+{
+    Console.WriteLine("Day 6 Problem 1");
+
+    var data = File.ReadAllLines("fish.txt");
+    var numDays = 80;
+    var fish = data[0].Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToList<int>();
+    PrintSchool(fish);
+    for(var num = 0; num < numDays; num++)
+    {
+        for(int idx = 0; idx < fish.Count; idx++)
+        {
+            if (fish[idx] == 0)
+            {
+                fish.Add(9);
+                fish[idx] = 7;
+            }
+            fish[idx]--;
+        }
+    }
+
+    Console.WriteLine($"Total fish is {fish.Count}");
+}
+
+static void ProblemTwo()
+{
+    Console.WriteLine("Day 6 Problem 2");
+
+    var data = File.ReadAllLines("fish.txt");
+    var numDays = 256;
+    var fish = data[0].Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToList<int>();
+    PrintSchool(fish);
+    for (var num = 0; num < numDays; num++)
+    {
+        for (int idx = 0; idx < fish.Count; idx++)
+        {
+            if (fish[idx] == 0)
+            {
+                fish.Add(9);
+                fish[idx] = 7;
+            }
+            fish[idx]--;
+        }
+    }
+
+    Console.WriteLine($"Total fish is {fish.Count}");
+    Console.WriteLine($"");
+}
+
+static void PrintSchool(List<int> fish)
+{
+    var output = String.Join(' ', fish);
+    Console.WriteLine(output);
+}
