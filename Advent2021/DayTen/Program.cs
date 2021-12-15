50c73b9385838ea775b067bba1bb968b6c6eee2c
Brian Nolan <bnolan001@gmail.com>
2021-12-14 22:38:22 -0500

    Completed day 10, problem 1, starting work on problem 2

---
+++ Advent2021/DayTen/Program.cs
@@ -0,0 +1,100 @@
+﻿using DayTen;
+// See https://aka.ms/new-console-template for more information
+
+ProblemOne();
+Console.WriteLine("-----------------------------");
+ProblemTwo();
+Console.WriteLine("-----------------------------");
+Console.WriteLine("Completed Day 10");
+
+static void ProblemOne()
+{
+    Console.WriteLine("Day 10 Problem 1");
+    var data = File.ReadAllLines("navigation.txt");
+    var badTokens = new List<Token>();
+
+    foreach(var line in data)
+    {
+        var parsedLine = ParseLine(line.Trim());
+        var badToken = ValidateTokens(parsedLine);
+        if (badToken != null)
+        {
+            badTokens.Add(badToken);
+        }
+    }
+    var total = badTokens.Sum(t => t.ErrorPoints);
+    Console.WriteLine($"Bad token points {total}");
+}
+
+static void ProblemTwo()
+{
+    Console.WriteLine("Day 10 Problem 2");
+    var data = File.ReadAllLines("navigation.txt");
+    var scores = new List<long>();
+    foreach (var line in data)
+    {
+        long score = 0;
+
+        var parsedLine = ParseLine(line.Trim());
+        var badToken = ValidateTokens(parsedLine);
+        if (badToken == null)
+        {
+            var remainingTokens = GetRemainingTokens(parsedLine);
+            remainingTokens.Reverse();
+            foreach(var token in remainingTokens)
+            {
+                score += (5 * score) + Token.GetById(token.Id + 1).ClosingPoints;
+            }
+            scores.Add(score);
+            
+        }
+    }
+    var middle = scores[scores.Count / 2];
+    Console.WriteLine($"Final score {middle}");
+}
+
+static List<Token> ParseLine(string line)
+{
+    var tokens = new List<Token>();
+    foreach(var token in line)
+    {
+        tokens.Add(Token.GetByValue(token));
+    }
+    return tokens;
+}
+
+static Token ValidateTokens(List<Token> tokens)
+{
+    var remainingTokens = new List<Token>();
+    foreach (var token in tokens)
+    {
+        if (token.Id % 2 == 0)
+        {
+            remainingTokens.Add(token);
+        }
+        else if (remainingTokens[remainingTokens.Count - 1].Id == token.Id - 1)
+        {
+            remainingTokens.RemoveAt(remainingTokens.Count - 1);
+        }
+        else { return token; }
+    }
+    return null;
+}
+
+static List<Token> GetRemainingTokens(List<Token> tokens)
+{
+    var remainingTokens = new List<Token>();
+    foreach (var token in tokens)
+    {
+        if (token.Id % 2 == 0)
+        {
+            remainingTokens.Add(token);
+        }
+        else if (remainingTokens[remainingTokens.Count - 1].Id == token.Id - 1)
+        {
+            remainingTokens.RemoveAt(remainingTokens.Count - 1);
+        }
+    }
+
+    return remainingTokens;
+}
---
+++ Advent2021/DayTen/Token.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTen
+{
+    public class Token
+    {
+        public static Token OpenParenthesis = new Token(0, "OpenParenthesis", '(', 0, 0);
+        public static Token ClosedParenthesis = new Token(1, "ClosedParenthesis", ')', 3, 1);
+        public static Token OpenSquareBracket = new Token(2, "OpenSquareBracket", '[', 0, 0);
+        public static Token ClosedSquareBracket = new Token(3, "ClosedSquareBracket", ']', 57, 2);
+        public static Token OpenSquiglyBracket = new Token(4, "OpenSquiglyBracket", '{', 0, 0);
+        public static Token ClosedSquiglyBracket = new Token(5, "ClosedSquiglyBracket", '}', 1197, 3);
+        public static Token LessThan = new Token(6, "LessThan", '<', 0, 0);
+        public static Token GreaterThan = new Token(7, "GreaterThan", '>', 25137, 4);
+
+        public int Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public char Value { get; private set; }
+
+        public int ErrorPoints { get; private set; }
+
+        public int ClosingPoints { get; private set; }
+
+        public Token(int value, string name, char token, int points, int closingPoints)
+        {
+            ErrorPoints = points;
+            Name = name;
+            Id = value;
+            Value = token;
+            ClosingPoints = closingPoints;
+        }
+
+        public static List<Token> GetList()
+        {
+            return new List<Token> { OpenParenthesis, ClosedParenthesis, 
+                OpenSquareBracket, ClosedSquareBracket, OpenSquiglyBracket, 
+                ClosedSquiglyBracket, LessThan, GreaterThan };
+        }
+
+        public static Token GetByValue(char token)
+        {
+            return GetList().FirstOrDefault(t => t.Value == token);
+        }
+
+        public static Token GetById(int id)
+        {
+            return GetList().FirstOrDefault(t => t.Id == id);
+        }
+    }
+}
