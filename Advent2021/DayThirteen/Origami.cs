c8c386d19806910e8b06f37e8ffe630edf3efb32
Brian Nolan <bnolan001@gmail.com>
2021-12-19 17:07:03 -0500

    Completed day 13, problem 1

---
+++ Advent2021/DayThirteen/Origami.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayThirteen
+{
+    public class Origami
+    {
+        public List<List<int>> Paper { get; set; } = new List<List<int>>();
+
+        public List<Coordinate> Coordinates { get; set; } = new List<Coordinate>();
+
+        public List<Coordinate> Folds { get; set; } = new List<Coordinate>();
+
+        public void CreatePaper()
+        {
+            var maxCols = Coordinates.Select(c => c.X).Max() + 1;
+            var maxRows = Coordinates.Select(c => c.Y).Max() + 1;
+            
+            for (int row = 0; row < maxRows; row++)
+            {
+                var paperRow = new List<int>();
+                for (int col = 0; col < maxCols; col++)
+                {
+                    paperRow.Add(0);
+                }
+                Paper.Add(paperRow);
+            }
+
+            foreach (var coord in Coordinates)
+            {
+                Paper[coord.Y][coord.X] = 1;
+            }
+        }
+
+        public void FoldPaper(Coordinate fold)
+        {
+            if (fold.X > 0)
+                FoldOnXAxis(fold.X);
+            else
+                FoldOnYAxis(fold.Y);
+
+            TrimPaperSize(fold);
+        }
+
+        private void FoldOnXAxis(int foldX)
+        {
+            var maxCols = Coordinates.Select(c => c.X).Max() + 1;
+
+            for (var col = foldX + 1; col < maxCols; col++)
+            {
+                for (var row = 0; row < Paper.Count; row++)
+                {
+                    var copyToX = foldX - (col - foldX);
+                    Paper[row][copyToX] = Paper[row][copyToX] + Paper[row][col];
+                }
+            }
+        }
+
+        private void FoldOnYAxis(int foldY)
+        {
+            var maxCols = Coordinates.Select(c => c.X).Max() + 1;
+
+            for (var row = foldY + 1; row < Paper.Count; row++)
+            {
+                var copyToY = foldY - (row - foldY);
+                for (var col = 0; col < Paper[row].Count; col++)
+                {                    
+                    Paper[copyToY][col] = Paper[copyToY][col] + Paper[row][col];
+                }
+            }
+        }
+
+        public void TrimPaperSize(Coordinate fold)
+        {
+
+            if (fold.Y > 0)
+            {
+                Paper.RemoveRange(fold.Y, Paper.Count - fold.Y);
+            }
+            else
+            {
+                for (var row = 0; row < Paper.Count; row++)
+                {
+                    Paper[row].RemoveRange(fold.X, Paper[row].Count - fold.X);
+                }
+            }
+        }
+
+    }
+    public record struct Coordinate
+    {
+        public int X { get; init; }
+        public int Y { get; init; }
+    }
+}
--- Advent2021/DayThirteen/Program.cs
+++ Advent2021/DayThirteen/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DayThirteen;
 
 ProblemOne();
 Console.WriteLine("-----------------------------");
@@ -10,12 +11,71 @@ static void ProblemOne()
 {
     Console.WriteLine("Day 13 Problem 1");
     var data = File.ReadAllLines("numbers.txt");
-    Console.WriteLine($"");
+    var origami = ParseData(data);
+    origami.CreatePaper();
+    origami.FoldPaper(origami.Folds[0]);
+    var totalDots = GetTotalDots(origami);
+    Console.WriteLine($"Total dots are {totalDots}");
 }
 
 static void ProblemTwo()
 {
     Console.WriteLine("Day 13 Problem 2");
     var data = File.ReadAllLines("numbers.txt");
+    var origami = ParseData(data);
     Console.WriteLine($"");
+}
+
+static Origami ParseData(string[] data)
+{
+    var origami = new Origami();
+    bool parsingPoints = true;
+    var coords = new List<Coordinate>();
+    for(int idx = 0; idx < data.Length; idx++)
+    {
+        if (parsingPoints)
+        {
+            if (String.IsNullOrEmpty(data[idx]))
+            {
+                parsingPoints = false;
+                continue;
+            }
+            var strCoords = data[idx].Split(',');
+            origami.Coordinates.Add(new Coordinate { X = int.Parse(strCoords[0]), Y = int.Parse(strCoords[1])}); 
+        }
+        else
+        {
+            int splitOn = data[idx].IndexOf('=');
+            if (splitOn > 0)
+            {
+                var fold = int.Parse(data[idx].Substring(splitOn + 1));
+                var coord = data[idx].Contains('x')? new Coordinate { X=fold, Y=0 }: new Coordinate { X = 0, Y = fold };
+                origami.Folds.Add(coord);
+            }
+        }
+    }
+    
+    return origami;
+}
+
+static void PrintPaper(Origami origami)
+{
+    //for (var row = origami.Paper.Count - 1; row >= 0; row--)
+    foreach(var row in origami.Paper)
+    {
+        Console.WriteLine(String.Join(" ", row));
+        //Console.WriteLine(String.Join(" ", origami.Paper[row]));
+    }
+    Console.WriteLine("----------------");
+}
+
+static int GetTotalDots(Origami origami)
+{
+    var totalDots = 0;
+    foreach(var row in origami.Paper)
+    {
+        totalDots += row.Count(d => d > 0);
+    }
+
+    return totalDots;
 }
